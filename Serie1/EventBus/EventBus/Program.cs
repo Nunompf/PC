525365f8441dcf258fa21a5adcbcf467540d258b
PTTANK <nuno_faiao@hotmail.com>
2018-10-19 16:50:56 +0100

    2 possívelmente feito

--- Serie1/EventBus/EventBus/Program.cs
+++ Serie1/EventBus/EventBus/Program.cs
@@ -9,6 +9,7 @@ namespace EventBus
     {
         int maxPending;
         bool shutdown;
+        bool isDone = false;
         Object monitor = new object();
         Dictionary<Type, LinkedList<Subscribers>> events = new Dictionary<Type, LinkedList<Subscribers>>();
 
@@ -19,63 +20,79 @@ namespace EventBus
         public void SubscribeEvent<T>(Action<T> handler) where T : class
         {
             Subscribers subs = new Subscribers(maxPending);
-            try
+            
+            lock (monitor)
             {
-                lock (monitor)
+                while (true)
                 {
-                    while (true)
+                    if (shutdown) return;
+                    if (!events.ContainsKey(typeof(T)))
                     {
-                        if (shutdown) return;
-                        if (!events.ContainsKey(typeof(T)))
-                        {
-                            events.Add(typeof(T), new LinkedList<Subscribers>());
+                        events.Add(typeof(T), new LinkedList<Subscribers>());
+                        events[typeof(T)].AddLast(subs);
+                    }
+                    else
+                    {
+                        if(events[typeof(T)].Count < subs.getMaxPending())
                             events[typeof(T)].AddLast(subs);
-                        }
-                        else
-                        {
-                            if(events[typeof(T)].Count < subs.getMaxPending())
-                                events[typeof(T)].AddLast(subs);
-                        }
-                        //try
+                    }
+                    try
+                    {
                         SyncUtils.Wait(monitor, subs.con);
                         subs.execute(handler);
-                        if(shutdown) //ir dic e rretirar subs 
+                        events[typeof(T)].Remove(subs);
+                        if (shutdown)
+                        {
+                            while(events[typeof(T)].Contains(subs));
+                            events[typeof(T)].Remove(subs);
+                            return;
+                        }
                     }
+                    catch(ThreadInterruptedException e)
+                    {
+
+                    }
+
                 }
             }
-            finally
-            {
-                throw new ThreadInterruptedException();
-            }
+            
+            
         }
         public void PublishEvent<E>(E message) where E : class
         {
-            try
+            lock (monitor)
             {
-                lock (monitor)
+                if (shutdown)
                 {
-                    foreach (var v in events)
+                    throw new InvalidOperationException();
+                }
+                foreach (var v in events)
+                {
+                    if (v.Key.Equals(typeof(E)))
                     {
-                        if (v.Key.Equals(typeof(E)))
+                        foreach(Subscribers s in v.Value)
                         {
-                           foreach(Subscribers s in v.Value)
-                            {
-                                s.message = message;
-                                SyncUtils.Notify(monitor, s.con);
-                            }
+                            s.message = message;
+                            SyncUtils.Notify(monitor, s.con); 
                         }
                     }
                 }
             }
-            finally
-            {
-                
-            }
+            
         }
         public void Shutdown()
         {
-            //dentro excl. mutua
-            shutdown = true;
+            lock (monitor)
+            {
+                shutdown = true;
+                while (events.Keys != null)
+                {
+                    foreach(var v in events.Keys)
+                    {
+                        if (events[v].Count == 0) events.Remove(v);
+                    }
+                }
+            }
         }
     }
 
